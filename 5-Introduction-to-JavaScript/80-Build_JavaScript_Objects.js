baf7eecaebe9b28d499e6cf8ea4a5da7bddfdc50
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-04 16:44:21 -0600

    Learn how to make objects with properties.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// Objects are a good way to make organized groups of data with properties instead of index numbers.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Lark",
+  "legs": 3,
+  "tails": 1,
+  "friends": ["cat", "teddy"]
+};
