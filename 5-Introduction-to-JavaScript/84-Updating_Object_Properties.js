e137204e29820839aa9f1b36d912b7ad3274c514
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-04 17:12:14 -0600

    Learn how to update properties with bracket or dot notation.

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,24 @@
+//We can update properties within an obkject with either bracket or dot notation the same way
+//we would update variable names.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.name = "Happy Coder";
