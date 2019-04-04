fb83c740d4a1915288f402ef5b584c63012875e6
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-03 23:17:46 -0600

    Learn strict equality opoerator ===

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,12 @@
+// === is strict equality, doesn't perform a type conversion
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
