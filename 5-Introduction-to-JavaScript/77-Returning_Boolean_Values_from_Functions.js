d054aa680207706468ceb6f299d7183496c40b3e
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-04 15:48:44 -0600

    Use comparison operators to get Boolean returns instead of always using if statements.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,10 @@
+//It's unnecessary to always use if statements to get true or false
+//Use Boolean
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+isLess(7, 8);
