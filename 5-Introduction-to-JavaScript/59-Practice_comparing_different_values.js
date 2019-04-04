906572b12fc45eac57791b66ee80fdc117ec1134
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-03 23:25:18 -0600

    Practice with strict equality operator ===

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+// strict equality means they have to be the same type as well as the same value
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
