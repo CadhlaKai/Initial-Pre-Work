b0cd9ee9d60d9a881a239698742302e8ac2afe31
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-04 17:08:18 -0600

    Learn how to use bracket notation with variables to call values from objects.

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,15 @@
+//We can create a variable that is the same as the property within an object,
+//then we can use that variable in bracket notation with the object to call the
+//value associated with the property.
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
