89c7d82cdee7438300d62b307f9c8b495bed33ea
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-04 16:48:08 -0600

    Learn how to access properties within an object with dot notation.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,14 @@
+//Learn how to access properties within an object with dot notation.
+//Even though the property is a string, I shouldn't write it as a string in dot notation.
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
