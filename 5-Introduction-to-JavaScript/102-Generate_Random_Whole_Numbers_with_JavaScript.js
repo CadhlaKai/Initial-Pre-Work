fb1baf788113877dbca2e001c8716b1153d2e634
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-05 00:16:48 -0600

    Learn how to get random whole numbers using Math.floor and Math.random.

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+//Learn how to get random whole numbers using Math.floor and Math.random.
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
