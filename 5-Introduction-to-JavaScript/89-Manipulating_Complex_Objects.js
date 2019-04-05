2ef9c5c27f0d70241c23a2f6f2b75732c47f511e
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-04 18:56:07 -0600

    Practice using objects to combine a variety of data by mimicking music metadata.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+//Objects allow for any compination of data to be stored together
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Meow",
+    "title": "Meow",
+    "release_year": 9999,
+    "formats": [
+      "Meow",
+      "Meow",
+      "Meow"
+    ],
+  }
+];
