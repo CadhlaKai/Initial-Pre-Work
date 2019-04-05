0e1d19252c37eb9f7f87c0c8c9288d2075d6b5f7
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-04 22:37:56 -0600

    Learn to use do while loops so that we can ensure that a function is performed at least once.

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...while loops.js
@@ -0,0 +1,10 @@
+//Change the while loop to a do while loop, ensuring that the function is performed at least once.
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
