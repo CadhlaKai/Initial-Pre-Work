0a686275f7c2d49d3ede5b8385b677400d949ec5
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-05 18:44:13 -0600

    default parameters can be placed into a function, this way there will always be a result instead of a traditional error.

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,11 @@
+//default parameters can be placed into a function
+//this way there will always be a result instead of a traditional error.
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
