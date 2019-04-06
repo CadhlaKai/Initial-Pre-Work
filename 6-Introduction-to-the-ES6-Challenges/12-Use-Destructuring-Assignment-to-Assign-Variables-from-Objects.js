cc64e9c667172b2b2399f57a76e496c004036c61
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-05 19:32:17 -0600

    We can deconstruct an array to get a specific value and assign it to a variable.

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,16 @@
+//We can deconstruct an array to get a specific value and assign it to a variable.
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow : tempOfTomorrow} = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
