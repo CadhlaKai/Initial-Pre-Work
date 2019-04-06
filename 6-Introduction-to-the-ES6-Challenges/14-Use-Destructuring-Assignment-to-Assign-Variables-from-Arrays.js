638e2a8a177ecd093330a94e94a3ed71a8549bc7
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-05 19:47:30 -0600

    Deconstructing arrays allows us to choose which elements we want to reassign.

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,11 @@
+//Deconstructing arrays allows us to choose which elements we want to reassign.
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [b, a] = [a, b];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
