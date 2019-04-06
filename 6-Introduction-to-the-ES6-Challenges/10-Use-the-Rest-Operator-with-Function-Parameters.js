dc0e8979fbc8a4e369a478ca7385ea63eb1da1d9
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-05 19:01:48 -0600

    The rest operator ... can be put as a prefix on the last parameter to indicate that infinite args can be given.

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,10 @@
+//the rest operator ... can be put as a prefix on the last parameter to indicate
+// that infinite args can be given.
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
