c35466a18d0d9cc13c58dde6afa9267151ed4f88
CadhlaKai <joshua.andrew.kiley@gmail.com>
2019-04-05 19:09:50 -0600

    Using the spread operator ... we can account for all elements in an array without typing them out individually.

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,10 @@
+// using the spread operator ... we can account for all elements in an array without
+// typing them out individually.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = []; // change this line
+})();
+console.log(arr2);
